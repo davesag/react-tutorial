d156d71f019f091b176c9074f909f3b42a9dbd48
davesag <davesag@gmail.com>
2015-08-13 17:15:24 -0400

    tutorials 1 thru 4 - heirarchy of models

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -8,6 +8,42 @@ var CommentBox = React.createClass({
     );
   }
 });
+
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// tutorial3.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+// moved from above (when it was in tutorial1)
 React.render(
   <CommentBox />,
   document.getElementById('content')
